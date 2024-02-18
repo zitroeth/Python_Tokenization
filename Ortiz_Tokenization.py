8441128dc55dcb01d9d47aa15f65b53c797b21ce
Ethan <ethan.ortiz.21@usjr.edu.ph>
2024-02-19 02:36:21 +0800

    Working

--- Ortiz_Tokenization.py
+++ Ortiz_Tokenization.py
@@ -1,7 +1,7 @@
 import re
 
 def tokenization(paragraph):
-    option = int(input("\n\n1. White Space\n2. Bigram\n3. Trigram\n4. Regex\nEnter option: "))
+    option = int(input("\n\n1. White Space\n2. Bigram\n3. Trigram\n4. Regex\n5. Change Paragraph\nEnter option: "))
     
     match option:
         case 1:
@@ -11,23 +11,25 @@ def tokenization(paragraph):
             tokens = []
             for i in range(0, len(word)-1):
                 tokens.append((word[i], word[i+1]))
-
         case 3:
             word = paragraph.split()
             tokens = []
             for i in range(0, len(word)-2):
                 tokens.append((word[i], word[i+1], word[i+2]))
         case 4:
-            tokens = "Error"
+            tokens = re.split("[ ,!?:;/]", paragraph)
+        case 5:
+            tokens = ''
         case _: 
             tokens = "Error"
     
-    return tokens
-        
-        
-# paragraph = input("Input Paragraph: ")
-paragraph = "Lorem ipsum dolor sit amet"
+    return tokens        
+
+#paragraph = "Lorem ipsum dolor sit amet"
+tokens = ''
 while(1):
+    if tokens == '':
+        paragraph = input("Input Paragraph: ")
     tokens = tokenization(paragraph)
     print(tokens)
     if(tokens == "Error"):
